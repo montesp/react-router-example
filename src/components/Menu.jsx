9f9303392659fad688472f1568535c7559d70111
Pavel Montes <pavelmontes22@gmail.com>
2023-02-14 21:51:51 -0600

    menu navlink component added

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,24 +1,54 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import './Menu.css';
+
+const routes = [];
+routes.push({
+  to: '/',
+  text: 'Home',
+});
+routes.push({
+  to: '/profile',
+  text: 'Profile',
+});
+routes.push({
+  to: '/blog',
+  text: 'Blog',
+});
+
 
 function Menu() {
   return (
     <header className="header">
       <nav>
         <ul>
-          <li>
-            <a href="/">Home</a>
+          {routes.map(route => (
+            <li>
+              <NavLink
+                to={route.to}
+                className={({ isActive })=> (
+                isActive ? 'link--selected':'link'
+              )}
+              >
+                {route.text}
+              </NavLink>
+            </li>
+          ))}
+          
+          {/* <li>
+            <Link to="/" className="link">Home</Link>
           </li>
           <li>
-            <a href="/#/profile">Profile</a>
+            <Link to="/blog" className="link">Blog</Link>
           </li>
           <li>
-            <a href="/#/blog">Blog</a>
-          </li>
+            <Link to="/profile" className="link">Profile</Link>
+          </li> */}
         </ul>
       </nav>
     </header>
   );
 }
+
 
 export { Menu };
