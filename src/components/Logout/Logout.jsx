740b07b4ecb08fbc3cfb1c082e872bb1fe972283
Pavel Montes <pavelmontes22@gmail.com>
2023-03-02 19:39:25 -0600

    logout styles created

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAuth } from "../../hooks/auth";
+import './Logout.css';
 
 function Logout() {
   const auth = useAuth();
@@ -10,13 +11,15 @@ function Logout() {
   };
 
   return (
-    <>
-      <h2>Logout</h2>
-      <form onSubmit={logout}>
-        <label id="user">¿Seguro  que quieres salir?</label>
-        <button type="submit">Salir</button>
-      </form>
-    </>
+    <main className="main">
+      <section className="logout">
+        <h2 className="logout__title">Logout</h2>
+        <form className="logout__form" onSubmit={logout}>
+          <label className="logout__form-text" id="user">¿Seguro  que quieres salir?</label>
+          <button className="logout__form-button" type="submit">Salir</button>
+        </form>
+      </section>
+    </main>
   );
 }
 
